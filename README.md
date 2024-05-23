7cb1ef65007e92a0764e0e06e3512bd6f0f3c5b7
Wayne Zhang <yxzwayne@gmail.com>
2024-05-23 14:47:27 -0700

    update:

---
+++ README.md
@@ -0,0 +1,9 @@
+# Starcraft II Ambience
+
+Pretty simple: I want the randomized experience that I set and forget while I work... That's it!
+
+# TODO
+- [ ] Add notifications for each race
+- [ ] Add mining ambience
+- [ ] Separate Broodwar and SC2 music and mining sound options
+- [ ] Add device action support (`F8`, `F9` on macOS)
---
+++ script.js
@@ -0,0 +1,136 @@
+const terranMusic = [
+    "audio/terran/music/terransc2-05.wav",
+    "audio/terran/music/terransc2-04.wav",
+    "audio/terran/music/zswarm_music_gs_terran9.wav",
+    "audio/terran/music/sc1_terran4.wav",
+    "audio/terran/music/terransc2-03.wav",
+    "audio/terran/music/sc1_terran1.wav",
+    "audio/terran/music/terransc2-02.wav",
+    "audio/terran/music/sc1_terran3.wav",
+    "audio/terran/music/sc1_terran2.wav",
+    "audio/terran/music/terransc2-01.wav",
+    "audio/terran/music/zswarm_music_gs terran8.wav",
+    "audio/terran/music/zswarm_music_terran6.wav",
+    "audio/terran/music/zswarm_music_terran7_action.wav"
+];
+
+const protossMusic = [
+    "audio/protoss/music/protosssc2-02.wav",
+    "audio/protoss/music/protosssc2-03.wav",
+    "audio/protoss/music/protosssc2-01.wav",
+    "audio/protoss/music/protosssc2-04.wav",
+    "audio/protoss/music/protosssc2-05.wav",
+    "audio/protoss/music/protosssc2-07.wav",
+    "audio/protoss/music/protosssc2-06.wav",
+    "audio/protoss/music/music_protosstakekievh.wav",
+    "audio/protoss/music/sc1_protoss4.wav",
+    "audio/protoss/music/sc1_protoss3.wav",
+    "audio/protoss/music/sc1_protoss2.wav",
+    "audio/protoss/music/sc1_protoss1.wav",
+]
+
+const zergMusic = [
+    "audio/zerg/music/zergsc2-06.wav",
+    "audio/zerg/music/sc1_zerg3.wav",
+    "audio/zerg/music/sc1_zerg2.wav",
+    "audio/zerg/music/zergsc2-07.wav",
+    "audio/zerg/music/zergsc2-05.wav",
+    "audio/zerg/music/sc1_zerg1.wav",
+    "audio/zerg/music/zergsc2-04.wav",
+    "audio/zerg/music/sc1_zerg4.wav",
+    "audio/zerg/music/zergsc2-01.wav",
+    "audio/zerg/music/zergsc2-03.wav",
+    "audio/zerg/music/zergsc2-02.wav",
+    "audio/zerg/music/music_zergrolloutd.wav",
+    "audio/zerg/music/zergsc2-09.wav",
+    "audio/zerg/music/zsm_zergadjutant_prekorhal01.wav",
+    "audio/zerg/music/zergsc2-08.wav",
+    "audio/zerg/music/zerg_intro_album_mix.wav",
+]
+
+let currentAudioIndex = 0;
+let isPlaying = false;
+let currentMusicCollection = terranMusic;
+const audioPlayer = document.getElementById('audioPlayer');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const nextButton = document.getElementById('next'); // Get the Next button
+const musicCollectionDropdown = document.getElementById('musicCollection');
+
+startButton.addEventListener('click', () => {
+    if (!isPlaying) {
+        isPlaying = true;
+        playAudio();
+    }
+});
+
+pauseButton.addEventListener('click', () => {
+    isPlaying = false;
+    audioPlayer.pause();
+});
+
+nextButton.addEventListener('click', () => {
+    currentAudioIndex = (currentAudioIndex + 1) % currentMusicCollection.length;
+    audioPlayer.src = currentMusicCollection[currentAudioIndex];
+    audioPlayer.play();
+    updateTrackName(); // Add this line to update the track name
+});
+
+audioPlayer.addEventListener('ended', () => {
+    if (isPlaying) {
+        playAudio();
+    }
+});
+
+musicCollectionDropdown.addEventListener('change', (event) => {
+    switch (event.target.value) {
+        case 'terran':
+            currentMusicCollection = terranMusic;
+            break;
+        case 'protoss':
+            currentMusicCollection = protossMusic;
+            break;
+        case 'zerg':
+            currentMusicCollection = zergMusic;
+            break;
+    }
+    if (isPlaying) {
+        playAudio();
+    }
+});
+
+function playAudio() {
+    currentAudioIndex = Math.floor(Math.random() * currentMusicCollection.length);
+    audioPlayer.src = currentMusicCollection[currentAudioIndex];
+    audioPlayer.play();
+    updateTrackName();
+}
+
+function updateTrackName() {
+    const trackNameElement = document.getElementById('trackName');
+    const currentTrackPath = currentMusicCollection[currentAudioIndex];
+    const currentTrackName = currentTrackPath.split('/').pop();
+    trackNameElement.textContent = currentTrackName;
+    toggleVisibility();
+}
+
+function toggleVisibility() {
+    const currentDiv = document.querySelector('.current');
+    const audioPlayer = document.getElementById('audioPlayer');
+    const trackNameElement = document.getElementById('trackName');
+    if (trackNameElement.textContent.trim() === '') {
+        currentDiv.style.display = 'none';
+        audioPlayer.style.display = 'none';
+    } else {
+        currentDiv.style.display = 'flex';
+        audioPlayer.style.display = 'block';
+    }
+}
+
+// Add this code to handle F9 key press
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'F9') {
+
+        document.getElementById('next').click();
+    }
+});
